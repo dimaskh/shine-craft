da6223003af0909e59e90892ab73a831e64d0196
dimaskh <45752663+dimaskh@users.noreply.github.com>
2024-12-18 17:24:19 +0000

    Deploying to gh-pages from @ dimaskh/shine-craft@6e2c7f7e56484f792ffee05fa4787b9dc65c092a 🚀

---
+++ _next/static/chunks/app/prices/page-f00c8291c9350d22.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[617,265],{34354:function(n,e,u){Promise.resolve().then(u.t.bind(u,68326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=34354)}),_N_E=n.O()}]);
